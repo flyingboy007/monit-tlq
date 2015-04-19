3622c220ceec087559894063ec93175916c5ee7a
Badr Tazi <tasibadr@gmail.com>
2015-04-20 00:04:15 +0100

    deleted using_mandrill attribute wasn't necessary

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,2 @@
 default[:monit][:notify_emails] = false
-default[:monit][:mailserver][:using_mandrill] = false
-default[:monit][:mailserver][:mail_format] = monit@myserver.com
+default[:monit][:mailserver][:mail_format] = "monit@myserver.com"
