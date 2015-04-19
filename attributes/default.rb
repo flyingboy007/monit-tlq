1d919fc3e145b53896b4a5725d183a724373a5c4
spawnge <tasibadr@gmail.com>
2015-04-19 23:03:59 +0100

    forgot strings

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
 default[:monit][:notify_emails] = false
 default[:monit][:mailserver][:using_mandrill] = false
-default[:monit][:mailserver][:mail_format] = monit@myserver.com
+default[:monit][:mailserver][:mail_format] = "monit@myserver.com"
